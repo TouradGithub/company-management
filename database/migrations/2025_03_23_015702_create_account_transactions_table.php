6cfc088af7e4e16c0e6d2198185b6bdf256e5287
TouradGithub <95051771+TouradGithub@users.noreply.github.com>
2025-03-23 22:20:45 +0100

    complate customers

---
+++ database/migrations/2025_03_23_015702_create_account_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('account_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id')->constrained()->onDelete('cascade');
+            $table->string('transaction_number')->unique();
+            $table->date('transaction_date');
+            $table->decimal('debit', 15, 2)->default(0);
+            $table->decimal('credit', 15, 2)->default(0);
+            $table->decimal('balance', 15, 2)->default(0);
+            $table->integer('company_id')->index();
+            $table->integer('branch_id')->nullable()->index();
+            $table->enum('source_type', ['JournalEntry', 'Invoice'])->default('JournalEntry');
+            $table->unsignedBigInteger('source_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('account_transactions');
+    }
+};
